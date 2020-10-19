d835ed58f308faa2019ba0203edc1f6a479c9d6b
olegpaliy <31694710+olegpaliy@users.noreply.github.com>
2020-10-19 21:26:01 +0300

    add 3 task

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ if(typeof e === 'string') {
 arr.push(e)
 }})
 console.log(arr)
+
+const arr3 =  [{name: "Yura", age: 55, hobby: ["football", "ski", "hiking"], type: "Admin"}, {name: "Yulian", age: 28, hobby: ["films", "games", "hiking"], type: "user"}, {name: "Taras", age: 38, hobby: ["hunting", "TV", "javascript"], type: "user"}];
+console.log(" users with type - user")
+arr3.forEach(item => {
+  if(item.type === "user") {
+    console.log(item.name)
+  }
+})
+console.log(" users with hobby - hiking")
+
+arr3.forEach(item => {
+  if(item.hobby.includes("hiking")) {
+    console.log(item.name)
+  }
+})
+
+arr3.forEach(item => {
+  item.job = true;
+})
+console.log(arr3);
